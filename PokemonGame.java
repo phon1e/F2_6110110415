53b55483ee047b5ffa3101ae9323a69d4fb41e53
phon1e <wiroon2543@gmail.com>
2020-02-08 14:49:04 +0700

    Update PokemonGame.java

--- PokemonGame.java
+++ PokemonGame.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class PokemonGame{
     public static void main(String[] args){
-        Pokemon p1 = new Pokemon("Eevee", 1, 12, 6, 6, 6); //ID LV HP Atk Def Speed
+        Pokemon p1 = new Pokemon("Eevee"); //ID 
         p1.displayPokemon();
-        Pokemon p2 = new Pokemon("Pikachu",1, 10, 5, 5, 5);
-        p2.displayPokemon();
+        
+        
     }
 }
